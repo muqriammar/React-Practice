c555e3b09727b99caaa2fb379806b62c814b46be
muqriammar <muqriammar8@gmail.com>
2024-02-11 00:15:28 +0800

    Small changes to css

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,18 +1,30 @@
+import React, { useState } from 'react';
+
+
 
 export function Container (){
+    const [buttonPressed, setButtonPressed] = useState(false)
+
+    const handleButtonClick = () => {
+        setButtonPressed(true);
+    }
 
    const handleClick = () => {
     alert("Hello");
    }
+
    
     return (
         <>
         <div className="main-container">
             <img src="src\images\photo.png"></img>
-            <h1>Hello</h1>
+            <h1>Will you be my Valentine?</h1>
             <div className="button-row">
             <button className="accept-button" onClick={handleClick}>Yes</button>
-            <button className="decline-button" onClick={handleClick}>No</button>
+            {!buttonPressed && (
+                <button className="decline-button" onClick={handleButtonClick}>No</button>
+            )}
+            
             </div>
         </div>
         </>
